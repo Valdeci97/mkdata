9b14e3a5daf560a7e131e7389c7f60f2b92de4a8
valdeci <moura97.valdeci@gmail.com>
2022-10-13 20:38:58 -0300

    build(repository): create corporate repository

---
+++ backend/src/main/java/com/mkdata/api/repositories/CorporateRepository.java
@@ -0,0 +1,11 @@
+package com.mkdata.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mkdata.api.models.Corporate;
+
+@Repository
+public interface CorporateRepository extends JpaRepository<Corporate, Long> {
+
+}
