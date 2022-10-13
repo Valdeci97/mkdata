f46e8e343c4ecdf466128dbe0463b428c49a009e
valdeci <moura97.valdeci@gmail.com>
2022-10-13 20:37:32 -0300

    build(repository): create individual repository

---
+++ backend/src/main/java/com/mkdata/api/repositories/IndividualRepository.java
@@ -0,0 +1,11 @@
+package com.mkdata.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mkdata.api.models.Individual;
+
+@Repository
+public interface IndividualRepository extends JpaRepository<Individual, Long> {
+
+}
