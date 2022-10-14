6b9e2dfa45db6e906f2d46f10df0c632048b0cb5
valdeci <moura97.valdeci@gmail.com>
2022-10-14 10:38:03 -0300

    refactor(srevices): apply dto methods

--- backend/src/main/java/com/mkdata/api/services/IndividualClientService.java
+++ backend/src/main/java/com/mkdata/api/services/IndividualClientService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mkdata.api.dto.IndividualClientDto;
 import com.mkdata.api.exceptions.ClientNotFoundException;
 import com.mkdata.api.models.Individual;
 import com.mkdata.api.repositories.IndividualRepository;
@@ -16,7 +17,7 @@ public class IndividualClientService {
 
   private final String clientNotFound = "Cliente não encontrado";
 
-  public Individual updateIndividualClient(Long id, Individual client) {
+  public Individual updateIndividualClient(Long id, IndividualClientDto client) {
     Optional<Individual> dbClient = individualRepository.findById(id);
     if (dbClient.isEmpty()) {
       throw new ClientNotFoundException(this.clientNotFound);
