daa8a1f4ef25dad2a4d0e7cb7962071f8a80634b
valdeci <moura97.valdeci@gmail.com>
2022-10-14 10:37:30 -0300

    build(api): create dto layer

---
+++ backend/src/main/java/com/mkdata/api/dto/CorporateClientDto.java
@@ -0,0 +1,29 @@
+package com.mkdata.api.dto;
+
+public class CorporateClientDto {
+  private String name;
+  private String club;
+  private String cnpj;
+  private String ie;
+  private String status;
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getClub() {
+    return this.club;
+  }
+
+  public String getCnpj() {
+    return this.cnpj;
+  }
+
+  public String getIe() {
+    return this.ie;
+  }
+
+  public String getStatus() {
+    return this.status;
+  }
+}
---
+++ backend/src/main/java/com/mkdata/api/dto/IndividualClientDto.java
@@ -0,0 +1,29 @@
+package com.mkdata.api.dto;
+
+public class IndividualClientDto {
+  private String name;
+  private String club;
+  private String cpf;
+  private String rg;
+  private String status;
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getClub() {
+    return this.club;
+  }
+
+  public String getCpf() {
+    return this.cpf;
+  }
+
+  public String getRg() {
+    return this.rg;
+  }
+
+  public String getStatus() {
+    return this.status;
+  }
+}
