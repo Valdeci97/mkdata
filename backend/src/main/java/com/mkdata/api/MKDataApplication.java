67d1113af929652f5b927a62613ebdd13503c755
valdeci <moura97.valdeci@gmail.com>
2022-10-15 18:07:11 -0300

    refactor(app): change main class name

---
+++ backend/src/main/java/com/mkdata/api/MKDataApplication.java
@@ -0,0 +1,13 @@
+package com.mkdata.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MKDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MKDataApplication.class, args);
+	}
+
+}
