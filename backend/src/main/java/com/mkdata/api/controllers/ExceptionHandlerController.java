b6171bc67a9f937978af8918212f328654fdfedc
valdeci <moura97.valdeci@gmail.com>
2022-10-14 23:13:17 -0300

    feat(controllers): create Exception handler controller

---
+++ backend/src/main/java/com/mkdata/api/controllers/ExceptionHandlerController.java
@@ -0,0 +1,18 @@
+package com.mkdata.api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.mkdata.api.exceptions.ClientAlreadyExistsException;
+import com.mkdata.api.exceptions.GlobalError;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+  @ExceptionHandler(ClientAlreadyExistsException.class)
+  public ResponseEntity<GlobalError> handleException(RuntimeException err) {
+    GlobalError message = new GlobalError(err.getMessage());
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(message);
+  }
+}
