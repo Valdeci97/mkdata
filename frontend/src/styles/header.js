5635efd4ee55fc7889a63080471d7e7c0e91ee3f
valdeci <moura97.valdeci@gmail.com>
2022-10-16 17:56:29 -0300

    style: create header style

---
+++ frontend/src/styles/header.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  align-items: center;
+  border-bottom: solid 1px #000;
+  display: flex;
+  font-size: 1.75rem;
+  height: 10vh;
+  justify-content: center;
+`;
