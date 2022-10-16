8da571110f017a77b9bcab8dcb0ccacdb1824869
valdeci <moura97.valdeci@gmail.com>
2022-10-16 17:48:30 -0300

    feat(Login): create header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from '../styles/header';
+
+export default function Header() {
+  return (
+    <S.HeaderContainer>
+      <h1>Client Manager</h1>
+    </S.HeaderContainer>
+  );
+};
