360a99fdb5817ba055cd0c9790e800fbd1d7f4fe
valdeci <moura97.valdeci@gmail.com>
2022-10-17 10:21:28 -0300

    style(create cliente): create style

---
+++ frontend/src/styles/createClient.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.form`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 80vh;
+`;
+
+export const Label = styled.label`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem 0;
+`;
+
+export const Input = styled.input`
+  border: solid 1px #000;
+  border-radius: 0.25rem;
+  height: 1.75rem;
+  margin: 0.25rem 0;
+  text-align: center;
+  width: 10rem;
+`;
+
+export const Select = styled.select`
+  background: transparent;
+  border-radius: 0.25rem;
+  height: 1.75rem;
+  margin: 0.25rem 0;
+  text-align: center;
+  width: 10rem;
+`;
