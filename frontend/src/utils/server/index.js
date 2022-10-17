37eba7780fbc01f42d4ab9dcd5622b695662b54d
valdeci <moura97.valdeci@gmail.com>
2022-10-17 16:38:17 -0300

    feat(server): create crud functions

--- frontend/src/utils/server/index.js
+++ frontend/src/utils/server/index.js
@@ -37,3 +37,65 @@ export async function createCorporateClient(client) {
     return null;
   }
 }
+
+export async function getClients() {
+  try {
+    const clients = await API.get();
+    return clients;
+  } catch (err) {
+    return null;
+  }
+}
+
+export async function getClientById(id) {
+  if (!id) return false;
+  try {
+    const client = await API.get(`/${id}`);
+    return client;
+  } catch (err) {
+    return null;
+  }
+}
+
+export async function destroyClient(id) {
+  if (!id) return false;
+  try {
+    await API.delete(`/${id}`);
+    return true;
+  } catch (err) {
+    return null;
+  }
+}
+
+export async function updateIndividualClient(id, client) {
+  if (!id) return false;
+  try {
+    const updatedClient = await API.put(`/pf/${id}`, {
+      name: client.name,
+      club: client.club,
+      cpf: client.cpf,
+      rg: client.rg,
+      status: client.status,
+    });
+    return updatedClient;
+  } catch (err) {
+    return null;
+  }
+}
+
+export async function updateCorporateClient(id, client) {
+  if (!id) return false;
+  try {
+    const updatedClient = await API.put(`/pj/${id}`, {
+      name: client.name,
+      club: client.club,
+      cnpj: client.cnpj,
+      ie: client.ie,
+      status: client.status,
+    });
+    return updatedClient;
+  } catch (err) {
+    return null;
+  }
+}
+
