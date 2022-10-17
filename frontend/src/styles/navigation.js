fac4455c8fe992840fc91e0429f52a1298110605
valdeci <moura97.valdeci@gmail.com>
2022-10-17 10:20:06 -0300

    style(navigation): create style

---
+++ frontend/src/styles/navigation.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const NavContainer = styled.nav`
+  align-items: center;
+  border-bottom: solid 1px #000;
+  display: flex;
+  justify-content: space-evenly;
+  height: 10vh;
+`;
+
+export const Link = styled.a`
+  color: #000;
+  font-size: medium;
+  font-weight: 500;
+  text-decoration: none;
+`;
