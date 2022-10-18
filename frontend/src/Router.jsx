929062b6f4c74bfb0cc4ccbf8c8fb0a5c3a3b39c
valdeci <moura97.valdeci@gmail.com>
2022-10-18 15:07:53 -0300

    feat(pages): create tech group page

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -4,6 +4,7 @@ import Clients from './pages/Clients';
 import CreateClient from './pages/CreateClient';
 import HealthGroup from './pages/HealthGroup';
 import Login from './pages/Login';
+import TechGroup from './pages/TechGroup';
 
 export default function Router() {
   return (
@@ -13,6 +14,7 @@ export default function Router() {
       <Route path="/create" element={ <CreateClient /> } />
       <Route path="/health" element={ <HealthGroup /> } />
       <Route path="/clients" element={ <Clients /> } />
+      <Route path="/tech" element={ <TechGroup /> } />
     </Routes>
   );
 };
