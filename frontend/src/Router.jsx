cf2cd964b07345bff15e0d769f131e8957f43531
valdeci <moura97.valdeci@gmail.com>
2022-10-18 15:12:53 -0300

    feat(pages): create finance group page

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Clients from './pages/Clients';
 import CreateClient from './pages/CreateClient';
+import FinanceGroup from './pages/FinanceGroup';
 import HealthGroup from './pages/HealthGroup';
 import Login from './pages/Login';
 import TechGroup from './pages/TechGroup';
@@ -15,6 +16,7 @@ export default function Router() {
       <Route path="/health" element={ <HealthGroup /> } />
       <Route path="/clients" element={ <Clients /> } />
       <Route path="/tech" element={ <TechGroup /> } />
+      <Route path="/finance" element={ <FinanceGroup /> } />
     </Routes>
   );
 };
